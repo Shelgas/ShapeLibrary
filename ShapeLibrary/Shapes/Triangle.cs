862e749ec6aa74a7ecf5e23b66761a1b048477aa
Shelgas <akozlov1997@gmail.com>
2024-06-17 11:13:36 +0300

    Triangle and Circle shapes implemented

---
+++ ShapeLibrary/Shapes/Triangle.cs
@@ -0,0 +1,34 @@
+﻿using ShapeLibrary.Intefaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary.Shapes
+{
+    public class Triangle : IShape
+    {
+        public double SideA { get; private set; }
+        public double SideB { get; private set; }
+        public double SideC { get; private set; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                throw new ArgumentException("Sides must be greater than zero.");
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+                throw new ArgumentException("The sum of any two sides must be greater than the third side.");
+
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double GetArea()
+        {
+            double P = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(P * (P - SideA) * (P - SideB) * (P - SideC));
+        }
+    }
+}
